d166440f44cfaf16d201aa857eb49895b91fccf9
sjiamnocna <jancikuvmail@gmail.com>
2022-08-01 21:26:15 +0200

    Added comments and errors for runcommand

--- src/CLIRunner/CLIRunner.class.php
+++ src/CLIRunner/CLIRunner.class.php
@@ -20,35 +20,55 @@ class CRunner
         $this->Container = $Container;
     }
 
+    /**
+     * Locks the runner from running multiple times at the same time
+     * @throws \Exception If the runner is already locked
+     */
     private function exec_lock(): void
     {
         if ($this->exec_is_locked()){
-            throw new \Exception('Tralala');
+            throw new \Exception('Cannot lock the lock');
         }
         $f = @fopen( self::lockFile, 'w' );
         fwrite($f, '1');
         fclose($f);
     }
 
+    /**
+     * Unlocks the runner for further use
+     * @return void
+     */
     private function exec_unlock(): void
     {
         unlink(self::lockFile);
     }
 
+    /**
+     * @return bool Tells whether the runner is already locked
+     */
     private function exec_is_locked(): bool
     {
         return file_exists(self::lockFile);
     }
 
+    /**
+     * Processes the CLI request
+     *
+     * @param array $argv Arguments from CLI
+     *
+     * @return void
+     *
+     * @throws \Throwable
+     */
     public function run(array $argv)
     {
         if ($this->exec_is_locked()){
-            throw new \Exception('Cannot run actions in parallel (yet)');
+            throw new \Exception('CLI is already running');
         }
-        // get filename
+        // make sure the CLI is running only from the runcommand.php file
         $p = pathinfo($argv[ 0 ]);
         if ($p['basename'] !== 'runcommand.php') {
-            throw new \Exception('Not allowed to run everywhere' . $argv[0], 403);
+            throw new \Exception(sprintf('Not allowed to run from `%s`', $argv[0]), 403);
         }
         $this->exec_lock();
 
